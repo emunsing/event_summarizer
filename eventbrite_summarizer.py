2d8fee2134e35a8db215656992daa4d6d53384fd
emunsing <eric.munsing@gmail.com>
2023-05-31 10:27:34 -0700

    Refactor and error handling

--- eventbrite_summarizer.py
+++ eventbrite_summarizer.py
@@ -22,13 +22,19 @@ def get_title_subtitle_from_event(event_url):
     tag_classes = {'title': 'event-title',
                    'subtitle': 'summary',
                    'details': 'has-user-generated-content'}
-    title = soup.find(class_ = tag_classes['title']).text
-    subtitle = soup.find(class_ = tag_classes['subtitle']).text
+    try:
+        title = soup.find(class_ = tag_classes['title']).text
+    except AttributeError as e:
+        raise AttributeError("No event title found on page; has it expired?")
+    
+    try:
+        subtitle = soup.find(class_ = tag_classes['subtitle']).text
+    except AttributeError as e:
+        raise AttributeError("No subtitle found on page; has it expired?")
+    
     return title, subtitle
 
-def get_event_details(event_url):
-    # Now get details:
-
+def get_eventbrite_json(event_url):
     event_id = get_event_id(event_url)
     headers = {'Authorization': 'Bearer {}'.format(EVENTBRITE_API_KEY)}
     params = {}
@@ -39,9 +45,12 @@ def get_event_details(event_url):
     effective_encoding =  'utf-8-sig' #r.apparent_encoding  #
     r.encoding = effective_encoding
 
-    res_tree = json.loads(r.text)
+    return json.loads(r.text)
 
-    content = res_tree['modules'][0]['data']['body']['text']
+def get_event_details(event_json):
+    # Now get details:
+
+    content = event_json['modules'][0]['data']['body']['text']
     content = content.replace('\ufeff', '')  # Remove byte order mark for utf-8-sig decoding.
 
     soup = BeautifulSoup(content, 'html.parser')
@@ -49,13 +58,31 @@ def get_event_details(event_url):
     details = soup.get_text(separator=' ')
     return details
 
-def get_eventbrite_summary(event_url, top_level_prompt_stub):
+def get_eventbrite_summary(event_url, top_level_prompt_stub, error_handling='attempt', modeltype="text-davinci-003", temperature=0.3):
+    """
+    error_handling in "skip", "attempt", 
+    """
+    errors = []
 
-    title, subtitle = get_title_subtitle_from_event(event_url)
-    details = get_event_details(event_url)
+    try:
+        title, subtitle = get_title_subtitle_from_event(event_url)
+    except AttributeError as e:
+        errors.append(e)
+        print(e)
+        title, subtitle = '', ''
 
-    temperature = 0.3
-    openai_modeltype = "text-davinci-003"
+    try:        
+        event_json = get_eventbrite_json(event_url)
+        details = get_event_details(event_json)
+    except AttributeError as e:
+        errors.append(e)
+        print(e)
+        details = ''
+
+    if errors and error_handling=='skip':
+        return 'Error'
+    
+    openai_modeltype = modeltype
 
     top_level_prompt = top_level_prompt_stub + """
     Event Title:{title}
